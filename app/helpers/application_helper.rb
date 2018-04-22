e0cf584af9ed16f8b5f6f1f5d32a31542d951811
asan kondo <htilcs1115@gmail.com>
2018-04-22 20:54:29 +0900

    github contributionsのデータを持ってくるメソッド作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def fetch_github_contributions_data
+    uri = URI.parse('https://github.com/users/k-nasa/contributions') # とりあえず自分ので固定
+    Net::HTTP.get(uri)
+  end
+
+  def parse_github_contributions_data
+    doc = Nokogiri::HTML.parse(fetch_github_contributions_data)
+    doc = doc.xpath("//rect[@data-date='#{Date.today.to_s}']") # 今日の分に絞り込む
+    doc = doc.first # データが１つだけのArrayになっているので最初だけを取り出す
+    doc.attribute('data-count').value.to_i
+  end
 end
